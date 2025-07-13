9cfc064f4ebe40f6e01a037bc832593a6f1b72e5
Gahaha <luke921021luke@gmail.com>
2025-07-13 22:51:43 +0800

    1.21.7 support

--- src/main/java/com/gahaha/entitycount/client/EntityCountClient.java
+++ src/main/java/com/gahaha/entitycount/client/EntityCountClient.java
@@ -5,14 +5,15 @@ import com.gahaha.entitycount.client.event.CountEntityEvent;
 import com.gahaha.entitycount.client.render.HudRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
 
 public class EntityCountClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
         ClientTickEvents.END_CLIENT_TICK.register(new CountEntityEvent());
-        HudLayerRegistrationCallback.EVENT.register(new HudRenderer());
+        HudElementRegistry.attachElementAfter(VanillaHudElements.SCOREBOARD, HudRenderer.ENTITY_COUNT_HUD_ID, new HudRenderer());
         //ClientTickEvents.END_CLIENT_TICK.register(new ScaleChangeEvent());
         ConfigManager.init();
     }
--- src/main/java/com/gahaha/entitycount/client/render/HudRenderer.java
+++ src/main/java/com/gahaha/entitycount/client/render/HudRenderer.java
@@ -7,29 +7,22 @@ import com.gahaha.entitycount.client.utils.EntityListFilter;
 import com.gahaha.entitycount.client.utils.MainSwitch;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
-import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
-import net.fabricmc.fabric.api.client.rendering.v1.LayeredDrawerWrapper;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElement;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.Map;
 
 @Environment(EnvType.CLIENT)
-public class HudRenderer implements HudLayerRegistrationCallback {
-    private static final Identifier entityCountLayer = Identifier.of("entitycount", "entitycount-layer");
+public class HudRenderer implements HudElement {
+    public static final Identifier ENTITY_COUNT_HUD_ID = Identifier.of("entitycount", "entitycount-hud");
 
     @Override
-    public void register(LayeredDrawerWrapper layeredDrawerWrapper) {
-        layeredDrawerWrapper.attachLayerAfter(IdentifiedLayer.SCOREBOARD, entityCountLayer, HudRenderer::render);
-
-    }
-    public static void render (DrawContext context, @Nullable RenderTickCounter tickCounter) {
+    public void render(DrawContext context, RenderTickCounter tickCounter) {
         if (!MainSwitch.canComputeAndRender()) return;
         renderDefault(context, EntityListFilter.getProcessedList(entityCountMap));
     }
@@ -40,9 +33,8 @@ public class HudRenderer implements HudLayerRegistrationCallback {
 
 
         // 取得 MatrixStack 並進行縮放
-        context.getMatrices().push();
-        context.getMatrices().scale(scale, scale, 1.0F);
-
+        context.getMatrices().pushMatrix();
+        context.getMatrices().scale(scale, scale);
 
         context.fill(tmpx, tmpy ,tmpx + 70, tmpy + (11*entryList.size()), ConfigManager.getBackgroundColor());
 
@@ -58,7 +50,7 @@ public class HudRenderer implements HudLayerRegistrationCallback {
             );
             tmpy += (int)(11); // 每列向下移動一點
         }
-        int a = ConfigManager.Default.textColor;
-        context.getMatrices().pop();
+
+        context.getMatrices().popMatrix();
     }
 }
