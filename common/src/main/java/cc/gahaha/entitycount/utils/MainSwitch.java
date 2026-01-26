5b2d292887c07e140813123a7765e980f09628c2
Gahaha <luke921021luke@gmail.com>
2026-01-27 03:29:00 +0800

    fix should show

--- common/src/main/java/cc/gahaha/entitycount/utils/MainSwitch.java
+++ common/src/main/java/cc/gahaha/entitycount/utils/MainSwitch.java
@@ -2,13 +2,19 @@ package cc.gahaha.entitycount.utils;
 
 import cc.gahaha.entitycount.config.ConfigManager;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.debug.DebugHudProfile;
 
 public class MainSwitch {
     private static final MinecraftClient client = MinecraftClient.getInstance();
 
+    public static boolean shouldShowDebugHudFix() {
+        DebugHudProfile profile = client.debugHudEntryList;
+        return profile.isF3Enabled() && (!client.options.hudHidden || client.currentScreen != null);
+    }
+
     public static boolean canComputeAndRender() {
         if (client.player == null || client.world == null) return false;
-        if (client.getDebugHud().shouldShowDebugHud()) return false;
+        if (shouldShowDebugHudFix()) return false;
         if (client.isPaused()) return false;
         return ConfigManager.isShowEntitiesCount();
     }
